7ff26b5d1dfa035bbe55d75bcdb344fa151ee529
antoine <antoineschilt@gmail.com>
2022-02-10 14:51:24 +0400

    :100:

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,74 @@ namespace cs_todo_list
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Todo> todos = new List<Todo>();
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadToDoList();
+        }
+        private void LoadToDoList()
+        {
+            todos = SQLiteDataAccess.LoadToDo();
+            WireUpToDoList();
+        }
+        private void BtnRefreshList_Click(object sender, RoutedEventArgs e)
+        {
+            LoadToDoList();
+        }
+        private void WireUpToDoList()
+        {
+            // attach the in-memory list to the ListBox
+            listToDoListBox.ItemsSource = null; // important to first make null
+            listToDoListBox.ItemsSource = todos;
+        }
+        private void BtnAddToDo_Click(object sender, RoutedEventArgs e)
+        {
+            Todo td = new Todo();
+            td.Name = txtboxName.Text;
+            td.Status = txtboxStatus.Text;
+
+            if (!string.IsNullOrEmpty(txtboxName.Text.Trim()) &&
+                  !string.IsNullOrEmpty(txtboxStatus.Text.Trim()))
+            {
+                SQLiteDataAccess.SaveToDo(td);
+                txtboxName.Text = "";
+                txtboxStatus.Text = "";
+            }
+        }
+        private void ListToDoListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox lb = sender as ListBox;
+            foreach (object o in lb.SelectedItems)
+            {
+                txtboxEditId.Text = (o as Todo).Id.ToString();
+                txtboxEditName.Text = (o as Todo).Name;
+                txtboxEditStatus.Text = (o as Todo).Status;
+            }
+        }
+        private void BtnSaveToDo_Click(object sender, RoutedEventArgs e)
+        {
+            var td = new Todo();
+            td.Id = Convert.ToInt32(txtboxEditId.Text);
+            td.Name = txtboxEditName.Text;
+            td.Status = txtboxEditStatus.Text;
+            SQLiteDataAccess.UpdateToDo(td);
+        }
+        private void BtnDeleteToDo_Click(object sender, RoutedEventArgs e)
+        {
+            var td = new Todo();
+            td.Id = Convert.ToInt32(txtboxEditId.Text);
+            td.Name = txtboxEditName.Text;
+            MessageBoxResult r = MessageBox.Show("Are you sure you want to permanently delete " + td.Name + "?", "Delete To Do",
+                MessageBoxButton.OKCancel, MessageBoxImage.Warning);
+            if (r == MessageBoxResult.OK)
+            {
+                SQLiteDataAccess.DeleteToDo(td.Id);
+                txtboxEditId.Text = "";
+                txtboxEditName.Text = "";
+                txtboxEditStatus.Text = "";
+            }
         }
     }
 }
---
+++ SQLiteDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Data.SQLite;
+
+namespace cs_todo_list
+{
+    class SQLiteDataAccess
+    {
+        public static List<Todo> LoadToDo()
+        {
+            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = conn.Query<Todo>("select * from Todos order by Id desc", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+        public static void SaveToDo(Todo todo)
+        {
+            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            {
+                conn.Execute("insert into Todos (Name, Status) values (@Name, @Status)", todo);
+            }
+        }
+        public static void UpdateToDo(Todo todo)
+        {
+            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            {
+                conn.Execute("update Todos set Name = @Name, Status = @Status where Id = @Id", todo);
+            }
+        }
+        public static void DeleteToDo(int todo)
+        {
+            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            {
+                conn.Execute("delete from Todos where id = @Id", new { Id = todo });
+            }
+        }
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
